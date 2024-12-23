6ff74aba0a3b19bf39649922dbfe0ff7be530355
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2024-12-22 19:09:40 -0800

    Initial Commit

---
+++ recure/recureApp.swift
@@ -0,0 +1,17 @@
+//
+//  recureApp.swift
+//  recure
+//
+//  Created by Gunhan Gulsoy on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct recureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
