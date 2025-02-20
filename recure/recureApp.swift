15aee275eb38464ca236f4fb4ffa80682e3e3fd4
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2025-02-19 23:24:52 -0800

    Attempt a fix for the data loading issues.

--- recure/recureApp.swift
+++ recure/recureApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct recureApp: App {
     @State private var reminders = Reminder.sampleData
     @State private var alerts = Alert.sampleData
+    @State private var isLoaded: Bool = false
     
     @StateObject private var reminder_store = ReminderStore()
     @StateObject private var alert_store = AlertStore()
@@ -28,23 +29,38 @@ struct recureApp: App {
                 }
             }
             .task {
+                // Load all data
                 do {
                     try await reminder_store.load()
                     try await alert_store.load()
-                    let center = UNUserNotificationCenter.current()
-                    do {
-                        try await center.requestAuthorization(options: [.alert, .sound, .badge])
-                    } catch {
-                        // Handle the error here.
-                        fatalError(error.localizedDescription)
-                    }
                 } catch {
+                    // No need to throw error, this is the first launch.
+                    print("First app launch")
+                }
+                self.isLoaded = true
+                
+                // Make sure we have notification permission.
+                let center = UNUserNotificationCenter.current()
+                do {
+                    try await center.requestAuthorization(options: [.alert, .sound, .badge])
+                } catch {
+                    // Handle the error here.
                     fatalError(error.localizedDescription)
                 }
             }
         }
         .backgroundTask(.appRefresh("Alert Refresh")) {
             await alert_store.refresh()
+        }
+        .backgroundTask(.appRefresh("Save State")) {
+            if await self.isLoaded {
+                do {
+                    try await reminder_store.save(reminders: reminder_store.reminders)
+                    try await alert_store.save(alerts: alert_store.alerts)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
